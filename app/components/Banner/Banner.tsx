5c7e11d16f8f087e2c9481f06ba71f6670883a34
Daniel Perez <dperezg23@outlook.com>
2025-07-29 13:31:52 -0500

    cambios banner

--- app/components/Banner/Banner.tsx
+++ app/components/Banner/Banner.tsx
@@ -8,26 +8,33 @@ const Banner = () => {
   const { openWhatsApp } = useWhatsApp();
   return (
     <main>
-      <div className="px-6 lg:px-8">
-        <div className="mx-auto max-w-7xl pt-16 sm:pt-20 pb-20 banner-image">
-          <div className="text-center">
-            <h1 className="text-4xl font-semibold text-navyblue sm:text-5xl  lg:text-7xl md:4px lh-96">
-              {t("title")}
-            </h1>
-            <p className="mt-6 text-lg leading-8 text-bluegray">
-              {t("subtitle")}
-            </p>
-          </div>
+      <div className="px-6 lg:px-8 ">
+        <div className="mx-auto max-w-7xl pt-16 sm:pt-20 pb-20 banner-image ">
+          <div className="max-h-screen">
+            <div className="text-center">
+              <h1 className="text-4xl font-semibold text-navyblue sm:text-5xl  lg:text-7xl md:4px lh-96">
+                {t("title")}
+              </h1>
+              <img
+                src="/imgs/logoFWMB.png"
+                alt="Banner"
+                className="mx-auto max-h-40 w-auto h-auto"
+                style={{ maxHeight: "160px" }}
+              />
+              <p className="mt-6 text-lg leading-8 text-bluegray">
+                {t("subtitle")}
+              </p>
+            </div>
 
-          <div className="text-center mt-5">
-            <button
-              type="button"
-              className="text-15px text-white font-medium bg-blue py-5 px-9 mt-2 leafbutton"
-              onClick={() => openWhatsApp("banner")}
-            >
-              {t("ctaPortfolio")}
-            </button>
-            
+            <div className="text-center mt-5">
+              <button
+                type="button"
+                className="text-15px text-white font-medium bg-blue py-5 px-9 mt-2 leafbutton"
+                onClick={() => openWhatsApp("banner")}
+              >
+                {t("ctaPortfolio")}
+              </button>
+            </div>
           </div>
 
           <Image
