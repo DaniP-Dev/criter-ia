c5514b4d6528e07359fd42a7347b22a52358b5db
Daniel Perez <dperezg23@outlook.com>
2025-07-29 13:53:29 -0500

    cambios banner v3

--- next.config.js
+++ next.config.js
@@ -14,7 +14,7 @@ const withNextIntl = createNextIntlPlugin({
 const isProd = process.env.NODE_ENV === 'production';
 
 const csp = isProd
-  ? "default-src 'self'; style-src 'self' fonts.googleapis.com; font-src 'self' fonts.gstatic.com; script-src 'self'; img-src 'self' data:;"
+  ? "default-src 'self'; style-src 'self' 'unsafe-inline' fonts.googleapis.com; font-src 'self' fonts.gstatic.com; script-src 'self' 'unsafe-inline'; img-src 'self' data:;"
   : "default-src 'self'; style-src 'self' 'unsafe-inline' fonts.googleapis.com; font-src 'self' fonts.gstatic.com; script-src 'self' 'unsafe-inline'; img-src 'self' data:;";
 
 const nextConfig = {
